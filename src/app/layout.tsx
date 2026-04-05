f214c6534ff17e21629a4e3d1dbeea0c1b6cbade
MedOussamaBraiek <oussemabraiek@gmail.com>
2026-04-05 13:38:26 +0200

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,33 @@ const dmMono = DM_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Oussama Braiek — Full Stack Developer",
+  title: "Oussama Braiek — Software Engineer",
   description:
     "Full Stack Developer & AI Engineer. Building fast, clean web & mobile applications.",
+  openGraph: {
+    title: "Oussama Braiek — Software Engineer",
+    description:
+      "Impression 3D de précision — figurines anime, Formula 1, décoration et plus.",
+    url: "https://braiek-mohamed-oussama.vercel.app/",
+    siteName: "Oussama Braiek — Software Engineer",
+    images: [
+      {
+        url: "https://braiek-mohamed-oussama.vercel.app/assets/oussama.jpg",
+        width: 1200,
+        height: 630,
+        alt: "FabrionLab",
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Oussama Braiek — Software Engineer",
+    description:
+      "Impression 3D de précision — figurines anime, Formula 1, décoration et plus.",
+    images: ["https://braiek-mohamed-oussama.vercel.app/assets/oussama.jpg"],
+  },
 };
 
 export default function RootLayout({
